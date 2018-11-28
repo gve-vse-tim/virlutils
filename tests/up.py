c0a9eaa9dc289f092315641b9eadf6c33a41fb70
Tim Miller <timmil@cisco.com>
2018-11-27 23:25:39 -0500

    Reduce the Python versions being tested for efficiencies sake.
    Disable exception catching in the invoke method.

--- tests/up.py
+++ tests/up.py
@@ -31,7 +31,7 @@ class Tests(BaseTest):
             net_url = 'http://localhost:19399/openstack/rest/networks'
             m.get(net_url, json=self.mock_os_net_response())
             runner = CliRunner()
-            result = runner.invoke(virl, ["up"])
+            result = runner.invoke(virl, ["up"], catch_exceptions=False)
             print("virl up result obj:" + str(result))
             self.assertEqual(0, result.exit_code)
 
